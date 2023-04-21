0ab584f999655d70d7c0e81b9a946f9eb288a4d9
Veronika Moskaliuk <veronika.moskaliuk.ua@gmail.com>
2023-04-21 13:58:49 +0200

    code correction

--- models/user.js
+++ models/user.js
@@ -2,8 +2,9 @@ const { Schema, model } = require("mongoose");
 const Joi = require("joi");
 const { handleMongooseError } = require("../utils");
 
-const emailRegexp = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
-const subscriptionList = ["starter", "pro", "business"];
+const {schemas} = require("../utils/validation/userValidationSchemas")
+
+const {emailRegexp, subscriptionList} = require("../utils/validation/userValidationSchemas");
 
 const userSchema = new Schema(
   {
@@ -35,23 +36,6 @@ const userSchema = new Schema(
 );
 
 userSchema.post("save", handleMongooseError);
-
-const registerSchema = Joi.object({
-  password: Joi.string().min(6).required(),
-  email: Joi.string().pattern(emailRegexp).required(),
-  subscription: Joi.string().valid(...subscriptionList),
-  token: Joi.string(),
-});
-
-const loginSchema = Joi.object({
-  email: Joi.string().pattern(emailRegexp).required(),
-  password: Joi.string().min(6).required(),
-});
-
-const schemas = {
-  registerSchema,
-  loginSchema,
-};
 
 const User = model("user", userSchema);
 
--- routes/api/authRoutes.js
+++ routes/api/authRoutes.js
@@ -4,16 +4,18 @@ const ctrl = require("../../controllers/authControllers");
 
 const { validateBody } = require("../../utils");
 
-const { schemas } = require("../../models/user");
+const { registerSchema } = require("../../utils/validation/userValidationSchemas");
+
+const { loginSchema } = require("../../utils/validation/userValidationSchemas");
 
 const {authenticate} = require("../../middlewares");
 
 const router = express.Router();
 
 //signup
-router.post("/register", validateBody(schemas.registerSchema), ctrl.register);
+router.post("/register", validateBody(registerSchema), ctrl.register);
 //singin
-router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
+router.post("/login", validateBody(loginSchema), ctrl.login);
 
 router.get("/current", authenticate, ctrl.getCurrent);
 
